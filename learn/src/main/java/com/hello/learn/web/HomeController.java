9480c48906037c4c3834b563c981f81d1c1396ba
Asraful <asraful.cse@gmail.com>
2023-05-29 12:25:33 +0300

    project creation API

--- learn/src/main/java/com/hello/learn/web/HomeController.java
+++ learn/src/main/java/com/hello/learn/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.hello.learn;
+package com.hello.learn.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ learn/src/main/java/com/hello/learn/web/ProjectController.java
@@ -0,0 +1,25 @@
+package com.hello.learn.web;
+
+import com.hello.learn.model.Project;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class ProjectController {
+
+    @GetMapping("/api/project/get")
+    public ResponseEntity<List<Project>> get() {
+        return new ResponseEntity<>(Collections.EMPTY_LIST, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/project/create")
+    public ResponseEntity<Project> create(@RequestBody Project project) {
+        System.out.println(project.getDescription());
+        return new ResponseEntity<>(new Project(), HttpStatus.OK);
+    }
+}
